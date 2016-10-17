c90d5deb3327994146cf84557a9521dfeb9d145c
Jeremy Schaub <schaubjeremy@gmail.com>
2016-10-17 15:20:51 -0500

    Added summary bar chart

--- app/js/charts.js
+++ app/js/charts.js
@@ -1,44 +1,93 @@
 'use strict';
 
 var timeseries_chart = function(data, divId, plotOptions) {
-        var chart = new Dygraph(
-            document.getElementById(divId),
-            data, plotOptions
-        );
-        return chart;
-    },
-    drawMonitor = function(monitor, source) {
+  var chart = new Dygraph(
+	document.getElementById(divId),
+	data, plotOptions
+  );
+  return chart;
+},
+  drawTimeseries = function(monitor, source) {
+	$.ajax({
+	  type: "GET",
+	  url: monitor[source].finalFilename,
+	  dataType: "text",
+	  success: function(data) {
+		var plotOptions = new TimeseriesPlotOptions,
+		  divId = monitor.divId;
+		plotOptions.title = monitor.title;
+		timeseries_chart(data, divId, plotOptions)
+	  },
+	  error: function(request, status, error) {
+		console.log(status);
+		console.log(error);
+	  }
+	});
+  },
+  calc_cumsum = function(data) {
+	var new_array = [],
+	  dt;
+	for (var i = 0; i < data.length; i++) {
+	  new_array.push(data[i].slice(0));
+	}
+	for (i = 1; i < new_array.length; i++) {
+	  dt = new_array[i][0] - new_array[i - 1][0];
+	  for (var j = 1; j < new_array[0].length; j++) {
+		new_array[i][j] = new_array[i - 1][j] + dt * new_array[i][j];
+	  }
+    }
+    return new_array;
+  },
+  summaryChart = function(measurements) {
+    // First create data object from elapsed time properties
+    var data = measurements.map(function(measurement){
+      return { x:measurement.run_id,
+        y:parseFloat(measurement.time.elapsed_time_sec)};
+    });
+    //console.log(data);
+    var chart = c3.generate({
+      bindto: '#id_summary',
+      //size: {
+        //height: 600
+      //},
+      data: {
+        json: data,
+        keys: {
+          x: 'x',
+          value: ['y']
+        },
+        names: {
+          y: 'Elapsed time [ sec ]'
+        },
+        type: "bar"
+      },
+      grid: {
+        x: {
+          show: true
+        },
+        y: {
+          show: true
+        }
+      },
+      point: {
+        r: 5
+      },
+      axis: {
+        x: {
+          type: 'category',
+          label: {
+            //text: xlabel,
+            position: 'outer-right'
+          }
+        },
+        y: {
+          min: 0,
+          label: {
+            text: 'Elapsed execution time [ seconds ]',
+            position: 'outer-middle'
+          }
+        },
+      }
+    });
+  };
 
-        // var url = measurements[currentMeasurement].monitors[monitor].data[currentSource].finalFilename;
-        // console.log('url is ' + url);
-        //console.log(monitor);
-        $.ajax({
-            type: "GET",
-            url: monitor[source].finalFilename,
-            dataType: "text",
-            success: function(data) {
-                var plotOptions = new TimeseriesPlotOptions,
-                    divId = monitor.divId;
-                plotOptions.title = monitor.title;
-                timeseries_chart(data, divId, plotOptions)
-            },
-            error: function(request, status, error) {
-                console.log(status);
-                console.log(error);
-            }
-        });
-    },
-    calc_cumsum = function(data) {
-        var new_array = [],
-            dt;
-        for (var i = 0; i < data.length; i++) {
-            new_array.push(data[i].slice(0));
-        }
-        for (i = 1; i < new_array.length; i++) {
-            dt = new_array[i][0] - new_array[i - 1][0];
-            for (var j = 1; j < new_array[0].length; j++) {
-                new_array[i][j] = new_array[i - 1][j] + dt * new_array[i][j];
-            }
-        }
-        return new_array;
-    };
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -20,76 +20,90 @@ vizWorkloadControllers.controller('summaryCtrl', ['$http', '$location',
 vizWorkloadControllers.controller('detailCtrl', ['$scope', '$routeParams', 
   '$http', '$location',
   function($scope, $routeParams, $http, $location) {
+    var counter = 0,
+      parseTimeFile = function(measurement){
+        $http.get(measurement.time.filename).success(function(data){
+          var hr=0,
+            min,
+            sec,
+            arr,
+            len;
+          arr = data.match(/([0-9]:)+[0-9]+\.+[0-9]+/)[0].split(':');
+          len = arr.length;
+          if (len === 3) {hr = parseInt(arr[0]);}
+          min = parseInt(arr[len-2]);
+          sec = parseFloat(arr[len-1]);
+          measurement.time.elapsed_time_sec = (hr*60*60 + min*60 + sec).toString();
+          measurement.time.exit_status = data.split('Exit status: ')[1].split('\n')[0];
+          measurement.time.exit_clean = measurement.time.exit_status === '0';
+          counter += 1;
+          updatePage();
+        })
+      },
+      updateDescription = function(){
+        $scope.measurements.forEach(function(meas){
+          // Update the description, etc to display this measurements attributes
+          if (meas.run_id == $scope.runId) {
+            $scope.description = meas.description;
+            $scope.timestamp = meas.timestamp;
+            $scope.hosts = meas.hosts;
+          }
+        });
+      },
+      loadChartData = function(runId){
+        $http.get(runId + '.json').success(function(chartdata){
+          $scope.measurement = chartdata;
+          // Create "allCharts"--an array of measurement detail objects 
+          // add allCharts to scope so that divs can be created prior to plotting
+          var keys = Object.keys(chartdata),
+            allCharts = [],
+            obj;
+          for (var i = 0; i < keys.length; i++) {
+            obj = $scope.measurement[keys[i]];
+            if (obj.hosts) {
+              //Only keys that have the 'hosts' field can be plotted
+              obj['divId'] = 'id_' + keys[i];
+              allCharts.push(obj);
+            }
+          }
+          $scope.allCharts = allCharts;
+          drawCharts();
+        })
+      },
+      drawCharts = function() {
+        for (var i = 0; i < $scope.allCharts.length; i++) {
+          drawTimeseries($scope.allCharts[i],
+            $scope.host);
+        }
+      },
+      checkURL = function(){
+        if ($scope.hosts.indexOf($scope.host) == -1){
+          // Bad URL.  Redirect to valid host
+          $scope.host = $scope.hosts[0];
+          var url = '/measurement/' + $scope.runId + '/' + $scope.hosts[0];
+          $location.path(url);
+          console.log('Redirect to ' + url);
+          return null;
+        }
+        return true;
+      },
+      updatePage = function(){
+        if (counter === $scope.measurements.length){
+          updateDescription();
+          if (checkURL()){
+            summaryChart($scope.measurements);
+            loadChartData($scope.runId);
+          }
+        }
+      };
+
     $scope.runId = $routeParams.runId;
     $scope.host = $routeParams.host;
 
-    function parseTimeFile(measurement){
-      $http.get(measurement.time.filename).success(function(data){
-        var hr=0,
-          min,
-          sec,
-          arr,
-          len;
-        arr = data.match(/([0-9]:)+[0-9]+\.+[0-9]+/)[0].split(':');
-        len = arr.length;
-        if (len === 3) {hr = parseInt(arr[0]);}
-        min = parseInt(arr[len-2]);
-        sec = parseFloat(arr[len-1]);
-        measurement.time.elapsed_time_sec = (hr*60*60 + min*60 + sec).toString();
-        measurement.time.exit_status = data.split('Exit status: ')[1].split('\n')[0];
-        measurement.time.exit_clean = measurement.time.exit_status === '0';
-      })
-    }
-
-    function loadChartData(runId){
-      $http.get(runId + '.json').success(function(chartdata){
-        $scope.measurement = chartdata;
-        // Create "allCharts"--an array of measurement detail objects 
-        // add allCharts to scope so that divs can be created prior to plotting
-        var keys = Object.keys(chartdata),
-          allCharts = [],
-          obj;
-        for (var i = 0; i < keys.length; i++) {
-          obj = $scope.measurement[keys[i]];
-          if (obj.hosts) {
-            //Only keys that have the 'hosts' field can be plotted
-            obj['divId'] = 'id_' + keys[i];
-            allCharts.push(obj);
-          }
-        }
-
-        $scope.allCharts = allCharts;
-        $scope.drawCharts();
-      }
-      );
-
-      $scope.drawCharts = function() {
-        for (var i = 0; i < $scope.allCharts.length; i++) {
-          drawMonitor($scope.allCharts[i],
-            $scope.host);
-        }
-      };
-    }
     // Add summary measurements to scope
     $http.get('summary.json').success(function(data){
       $scope.measurements = data;
       $scope.measurements.forEach(parseTimeFile);
-      $scope.measurements.forEach(function(meas){
-        if (meas.run_id == $scope.runId) {
-          $scope.description = meas.description;
-          $scope.timestamp = meas.timestamp;
-          $scope.hosts = meas.hosts;
-        }
-      });
-      if ($scope.hosts.indexOf($scope.host) == -1){
-        // Redirect to valid host
-        $scope.host = $scope.hosts[0];
-        var url = '/measurement/' + $scope.runId + '/' + $scope.hosts[0];
-        $location.path(url);
-        console.log('Redirect to ' + url);
-      }
-      loadChartData($scope.runId);
     });
-
   }
 ]);

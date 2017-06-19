7463b58002158958878a32fe2961abe072c38909
Jeremy Schaub <schaubjeremy@gmail.com>
2017-06-19 12:10:18 -0400

    Simplified example and renamed raw nvprof file

--- scripts/parse-monitor.sh
+++ scripts/parse-monitor.sh
@@ -11,5 +11,13 @@ FN=${DIR}/${RUN_ID}.${HOST}.${MONITOR}
 [ $MONITOR == "interrupts" ] && ./parse_interrupts.py $FN
 [ $MONITOR == "gpu" ] && ./parse_gpu.py $FN
 [ $MONITOR == "pcie" ] && ./parse_pcie.py $FN
-[ $MONITOR == "nvprof" ] && ./parse_nvprof.py $FN
+if [ $MONITOR == "nvprof" ]; then
+    # nvprof generates 1 or more files, and adds the PID to the filename
+    # pick the largest file and rename it so that the raw file can be 
+    # downloaded from the web page
+    RAW_FN=`ls -S ${FN}* | head -n 1`
+    echo Renaming $RAW_FN to $FN
+    mv $RAW_FN $FN
+    ./parse_nvprof.py $FN
+fi
 
